1514a58664fcd9ca3492e22c7cf7cf261efc8a03
Tianyu Liu <tyliu1122@gmail.com>
2024-10-17 01:58:04 +1100

    Env config

---
+++ test.py
@@ -0,0 +1,15 @@
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()
+
+
+# poetry run python test.py
